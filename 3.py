7748cb12ea901c468f3326cd093adc6b6d2d4d99
Priyam Ghosh <priyamghosh2009@outlook.com>
2026-09-01 11:46:58 +0530

    All the three Rotations in Cv

---
+++ 3.py
@@ -0,0 +1,11 @@
+import cv2
+img=cv2.imread('assets/1.png',-1)
+img=cv2.resize(img,(0,0),fx=0.09,fy=0.09)
+img=cv2.rotate(img,cv2.ROTATE_90_COUNTERCLOCKWISE)
+img2=cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+img3=cv2.rotate(img,cv2.ROTATE_180)
+cv2.imshow("Priyam_90CC",img)
+cv2.imshow("Priyam_90C",img2)
+cv2.imshow("Priyam_180",img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
